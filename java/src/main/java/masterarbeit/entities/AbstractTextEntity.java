9ca899a2c86045017419e5d11d752e4bc8806c93
Peter Garscha <peter.garscha@gmail.com>
2020-10-14 10:49:28 +0200

    clean up

--- java/src/main/java/masterarbeit/entities/AbstractTextEntity.java
+++
@@ -1,9 +0,0 @@
-package masterarbeit.entities;
-
-import java.util.List;
-
-public abstract class AbstractTextEntity {
-
-	abstract public List<? extends AbstractGoogleWordEntity> getGoogleWordEntities();
-
-}
